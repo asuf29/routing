499b34296238a7a5e00a01906000618e89572125
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-02-22 15:59:39 +0300

    loading added

--- src/components/User.js
+++ src/components/User.js
@@ -3,19 +3,22 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function User() {
+  const [loading, setLoading] = useState(true);
+
   const [user, setUser] = useState({});
 
   const { id } = useParams();
 
   useEffect(() => {
-    axios(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) =>
-      setUser(res.data)
-    );
+    axios(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => setUser(res.data))
+      .finally(() => setLoading(false));
   }, [id]);
 
   return (
     <div>
       <h1>User Details</h1>
+      {loading && <div>Loading...</div>}
       <code>{JSON.stringify(user)}</code>
     </div>
   );
