09a7149b19068fc0ef0722e55504168f64ca87db
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-02-22 15:51:50 +0300

    axios used

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function User() {
-  const { id } = useParams;
+  const [user, setUser] = useState({});
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) =>
+      setUser(res.data)
+    );
+  }, [id]);
+
   return (
     <div>
       <h1>User Details</h1>
-      id: {id}
+      <code>{JSON.stringify(user)}</code>
     </div>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -4,19 +4,22 @@ import axios from "axios";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users").then((res) =>
+      setUsers(res.data)
+    );
+  });
+
   return (
     <div>
       <h1>Users</h1>
       <ul>
-        <li>
-          <Link to="/user/1">User 1</Link>
-        </li>
-        <li>
-          <Link to="/user/2">User 2</Link>
-        </li>
-        <li>
-          <Link to="/user/3">User 3</Link>
-        </li>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/user/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
